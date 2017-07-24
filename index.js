d3ba6f752381d0b513b5666b20053626aea71014
胡钺 <shixi_huyue3@staff.sina.com.cn>
2017-07-24 11:41:47 +0800

    cp .

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const spinner = ora("downloading the latest bicycle scheme").start();
 
 exec("cd .. && git clone https://github.com/flypie2/bicycle-pack.git", () => {
   spinner.text = "copy scheme to your project";
-  exec("rm -rf ../bicycle-pack/.git && cp -rf ../bicycle-pack/* ./", () => {
+  exec("rm -rf ../bicycle-pack/.git && cp -rf ../bicycle-pack/. ./", () => {
     exec("rm -rf ../bicycle-pack", () => {
       spinner.stop();
     });
